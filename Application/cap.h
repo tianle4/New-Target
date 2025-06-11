ea05df399c116285c12e3e129126767083eee93d
Goldstein <tianle4@outlook.com>
2025-06-11 23:19:58 +0800

    添加小陀螺变速和变速模式
    修改文件：chassis.c
    8-9行:添加小陀螺转速ConstantValue，变速标志位flag
    135-150：右杆中间为Follow，下拨小陀螺，上拨变速
    152-171：小陀螺变速编写
    424-432：小陀螺添加转向，添加变速模式

---
+++ Application/cap.h
@@ -0,0 +1,14 @@
+#ifndef CAP_H
+#define CAP_H
+#include "includes.h"
+
+typedef struct
+{
+    float Voltage;
+    float Power;
+    uint8_t Cap_Normol_Chassis_Flag;
+} cap_t;
+
+void Cap_unused_correct(void);
+extern cap_t Cap;
+#endif
